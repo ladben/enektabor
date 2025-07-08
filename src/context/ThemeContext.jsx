4666d82bda7d5c97c3579e8ea2f25e5ccc5e6e88
Ladányi Bence <ladanyi.bence96@gmail.com>
2025-07-08 23:23:37 +0200

    routing added

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState, useContext } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useTheme = () => useContext(ThemeContext);
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ItemProvider } from './context/ItemContext.jsx'
+import { ItemProvider } from './context/ItemContext.jsx';
+import { ThemeProvider } from './context/ThemeContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ItemProvider>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </ItemProvider>
   </StrictMode>,
 )
