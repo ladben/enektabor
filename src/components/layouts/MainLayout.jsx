7dc96776c586f81a5c727333c0dedc2d4b17de79
Ladányi Bence <ladanyi.bence96@gmail.com>
2026-06-19 17:35:07 +0200

    reduce img size when not on homescreen

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { useLocation, Outlet } from 'react-router-dom';
 import { useState } from 'react';
 import LogoutModal from '../modals/LogoutModal';
 import { AnimatePresence } from 'framer-motion';
 import { useUser } from '../../context/UserContext';
 
 const MainLayout = () => {
+  const location = useLocation();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { user } = useUser();
+
+  const isHomePage = location.pathname === '/';
 
   const handleImageClick = () => {
     user && setIsModalOpen(true);
@@ -15,7 +18,12 @@ const MainLayout = () => {
   return (
     <div className='flex flex-column h-100sv of-hidden'>
       <div className='head-image flex px-50 flex-justify-center flex-align-center elevation-section-shadow'>
-        <img src='/header_img.png' alt="" className='w-100' onClick={handleImageClick} />
+        <img
+          src='/header_img.png'
+          alt=''
+          className={isHomePage ? 'w-100' : 'w-50'}
+          onClick={handleImageClick}
+        />
       </div>
       <div className='main-wrapper flex flex-column gap-24 p-32 flex-align-center of-hidden h-100'>
         <Outlet />
@@ -23,12 +31,12 @@ const MainLayout = () => {
 
       {/* Modal */}
       <AnimatePresence>
-        { isModalOpen && user && (
+        {isModalOpen && user && (
           <LogoutModal onClose={() => setIsModalOpen(false)} />
         )}
       </AnimatePresence>
     </div>
   );
-}
- 
+};
+
 export default MainLayout;
